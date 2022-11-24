213254336e3c8f87159e9a128d297dd7dc330c6e
aworkconsulting <emelie@aworkconsulting.se>
2022-11-24 10:51:35 +0100

    added dictionary.js and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./aworklogo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>Coded by Emelie</small>
+        </footer>
+      </div>
     </div>
   );
 }
