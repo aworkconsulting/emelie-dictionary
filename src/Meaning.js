a7457c6f148a25e183defdbf019dfa236056d4c1
aworkconsulting <emelie@aworkconsulting.se>
2022-11-25 14:28:42 +0100

    Added synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,15 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
+              <br />
+              <strong>Example: </strong>
               <em> {definition.example}</em>
+              <br />
+              <strong>Synonyms: </strong>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
