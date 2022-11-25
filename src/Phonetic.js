d05a6de47406e5fbdba72b40a682bc5a6ebb3b15
aworkconsulting <emelie@aworkconsulting.se>
2022-11-25 15:54:28 +0100

    changed some css

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,14 +2,12 @@ import React from "react";
 
 export default function Phonetic(props) {
   return (
-    <section>
-      <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          Listen
-        </a>
-
-        {props.phonetic.text}
-      </div>
-    </section>
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
@@ -8,23 +9,21 @@ export default function Results(props) {
       <div className="Results">
         {" "}
         <section>
-          {" "}
           <h1>{props.results.word}</h1>
+
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
         </section>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
-              <section>
-                <Meaning meaning={meaning} />
-              </section>
-            </div>
+            <section key={index}>
+              <Meaning meaning={meaning} />
+            </section>
           );
         })}
       </div>
