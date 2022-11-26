3fafa47e5f9b437c18fd5c801ad1cb7e634339cb
aworkconsulting <emelie@aworkconsulting.se>
2022-11-26 12:50:50 +0100

    changed some missspellings

--- src/Photos.js
+++ src/Photos.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Photos.css";
 
-export default function photos(props) {
+export default function Photos(props) {
   if (props.photos) {
     return (
-      <section className="photos">
+      <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img scr={photo.src.landscape} alt="" className="img-fluid" />
+                  <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
             );
